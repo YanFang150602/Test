fa4b0b07aac1200a016fc8888c289b2030f44fc3
YanFang <838641570@qq.com>
2020-09-13 17:01:26 +0800

    string toCharArray

---
+++ JavaBase/src/main/java/com/test/javabase/string/StringToCharArray.java
@@ -0,0 +1,11 @@
+package com.test.javabase.string;
+
+public class StringToCharArray {
+
+    public static void main(String[] args) {
+        String Str = new String("www.runoob.com");
+
+        System.out.print("返回值 :" );
+        System.out.println( Str.toCharArray() );
+    }
+}
